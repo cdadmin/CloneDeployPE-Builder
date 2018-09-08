f861e677d64830100471be09d53a585d89261642
cdadmin <cdadmin@clonedeploy.org>
2018-09-07 23:39:21 -0400

    add ssh option to winpe

--- Optional/ExtraFiles/WinPE 10/Windows/System32/startnet.cmd
+++ Optional/ExtraFiles/WinPE 10/Windows/System32/startnet.cmd
@@ -1,3 +1,6 @@
 wpeinit
+wpeutil disablefirewall
 powershell Set-ExecutionPolicy bypass
+powershell -Command "New-Service -Name sshd -BinaryPathName x:\windows\system32\sshd.exe"
+net start sshd
 powershell x:\windows\system32\wie_start.ps1
